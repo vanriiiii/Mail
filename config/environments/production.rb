b56353502e9725a2111cf0da68aebd3762185b51
vanri <vanri0415@gmail.com>
2019-02-05 21:32:24 +0900

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'thawing-scrubland-61541.herokuapp.com' }
    ActionMailer::Base.delivery_method = :smtp
    ActionMailer::Base.smtp_settings = {
